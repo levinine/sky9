37d6d841e02a37ffbbb0f69202bc6da7acb181a7
zelenins <zelenovicborisa@gmail.com>
2019-11-07 09:44:15 +0100

    try catch added

--- src/handler/account-handler.js
+++ src/handler/account-handler.js
@@ -3,20 +3,32 @@ const accountService = require('../service/account-service');
 const responseHandler = require('../../infrastructure/response-handler');
 
 const getAccount =  async event => {
-  const id = event.pathParameters.id;
-  const account = await accountService.getAccount(id)
-  return responseHandler(account);
+  try{
+    const id = event.pathParameters.id;
+    const account = await accountService.getAccount(id)
+    return responseHandler(account);
+  } catch(error) {
+    //error handler
+  }
 };
 
 const getAccounts = async () => {
-  const accounts = await accountService.getAccounts();
-  return responseHandler(accounts);
+  try {
+    const accounts = await accountService.getAccounts();
+    return responseHandler(accounts);
+  } catch(error) {
+    //error handler
+  }
 };
 
 const createAccount= async event => {
-  const data = JSON.parse(event.body);
-  const account = await accountService.createAccount(data);
-  return responseHandler(account);
+  try{
+    const data = JSON.parse(event.body);
+    const account = await accountService.createAccount(data);
+    return responseHandler(account);
+  } catch(error) {
+    //error handler
+  }  
 }
 
 module.exports = {
