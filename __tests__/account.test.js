a70488b8a6ae78a9078ac08263a3bf898eb03ecc
zelenins <zelenovicborisa@gmail.com>
2019-11-07 15:51:06 +0100

    Added Create Account API Test

--- __tests__/account.test.js
+++ __tests__/account.test.js
@@ -1,21 +1,79 @@
 /* eslint-disable no-undef */
 const request = require('supertest');
-let accountID = null;
+const testAccount = {
+    "email": "test@email.com",
+    "name": "test skripta",
+    "status": "inactive",
+    "IAMUsers": [
+        "user3",
+        "user2",
+        "user6"
+    ]
+}
+let testGetAccount = {};
+
+describe('Create Account API Test', () => {
+    it('should return the created account', async () => {
+        const res = await request(process.env.API_ENDPOINT)
+        .post('/accounts/')
+        .send(testAccount);
+        const account = JSON.parse(res.text);
+        testGetAccount = account;
+        expect(res.statusCode).toEqual(200)
+        expect(account.name).toEqual(testAccount.name)
+        expect(account.email).toEqual(testAccount.email)
+    });
+});
 
 describe('Get Accounts API Test', () => {
-    it('should return all users', async () => {
-      const res = await request(process.env.API_ENDPOINT).get('/accounts').send()
+    it('should return all accounts', async () => {
+      const res = await request(process.env.API_ENDPOINT)
+      .get('/accounts')
+      .send()
       expect(res.statusCode).toEqual(200)
-      accountID = (JSON.parse(res.text).Items[0].id);
-      console.log(accountID);
+      const accounts = JSON.parse(res.text).Items;
+      expect(Array.isArray(accounts)).toEqual(true)
+      accounts.forEach((account) => {
+          expect(typeof account).toEqual('object')
+          expect(Object.keys(account)).toEqual('id')
+
+          expect(typeof account.name).toEqual('string');
+          expect(typeof account.email).toEqual('string');
+          expect(typeof account.status).toEqual('string');
+      })
     })
   })
 
+
+  describe('myDataSource.json', () => {
+    it('Should contain an array of objects matching the intended shape.', () => {
+      expect(Array.isArray(myDataSource)).toEqual(true);
+      myDataSource.forEach((dataPoint) => {
+          
+        expect(typeof dataPoint).toEqual('object');
+        expect(Object.keys(dataPoint).sort()).toEqual([
+          'description',
+          'footnotes',
+          'title',
+          'value',
+        ]);
+   
+        expect(typeof dataPoint.description).toEqual('string');
+        expect(typeof dataPoint.title).toEqual('string');
+        expect(typeof dataPoint.value).toEqual('number');
+      });
+    });
+  });
+
 describe('Get Account API Test', () => {
-    it('should return a given user', async () => {
-        const res = await request(process.env.API_ENDPOINT).get('/accounts/' + accountID ).send();
+    it('should return a given account', async () => {
+        const res = await request(process.env.API_ENDPOINT)
+        .get('/accounts/' + testGetAccount.id )
+        .send();
         const account = JSON.parse(res.text).Item;
         expect(res.statusCode).toEqual(200)
-        expect(account.id).toEqual(accountID);
-    })
-})
+        expect(account.id).toEqual(testGetAccount.id);
+    });
+});
+
+
