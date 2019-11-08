5ff529adb90b6d28aa19358f2aa853eb1286bc26
zelenins <zelenovicborisa@gmail.com>
2019-11-08 10:19:59 +0100

    update account handler added

--- src/handler/account-handler.js
+++ src/handler/account-handler.js
@@ -3,7 +3,7 @@ const accountService = require('../service/account-service');
 const errorHandler = require('../../infrastructure/error-handler');
 const responseHandler = require('../../infrastructure/response-handler');
 
-const getAccount =  async event => {
+const GetAccount =  async event => {
   try{
     const id = event.pathParameters.id;
     const account = await accountService.getAccount(id)
@@ -13,7 +13,7 @@ const getAccount =  async event => {
   }
 };
 
-const getAccounts = async () => {
+const GetAccounts = async () => {
   try {
     const accounts = await accountService.getAccounts();
     return responseHandler(accounts);
@@ -22,19 +22,29 @@ const getAccounts = async () => {
   }
 };
 
-const createAccount= async event => {
+const CreateAccount= async event => {
   try{
     const data = JSON.parse(event.body);
     const account = await accountService.createAccount(data);
     return responseHandler(account);
   } catch(error) {
     return errorHandler(error);
-  }  
+  }
+}
 
+const UpdateAccount = async event => {
+  try {
+    const data = JSON.parse(event.body);
+    const account = await accountService.UpdateAccount(data);
+    return responseHandler(account);
+  } catch(error) {
+    return errorHandler(error);
+  }
 }
 
 module.exports = {
-  getAccount,
-  getAccounts,
-  createAccount
+  GetAccount,
+  GetAccounts,
+  CreateAccount,
+  UpdateAccount
 };
