d12ebfc2b8754e3cc792fefc9ff8a699e13e9a01
markoninho <sarcevic.marko94@gmail.com>
2019-11-18 16:08:26 +0100

    pageNotFound

---
+++ client/src/components/ErrorNotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class ErrorNotFound extends Component {
+
+    render() {
+        return (
+            <div id='error'>
+                <h1 className="notFoundTitle" align='center'>404</h1>
+                <h2 className="notFoundTitle" align='center'> page not found</h2>
+                
+            </div>
+        );  
+    }
+}
